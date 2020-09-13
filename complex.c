05c1a0d9c3e5eaa8853a3a1ec3de38928738e592
dan-taranetc <71114728+dan-taranetc@users.noreply.github.com>
2020-09-13 15:17:03 +0700

    Ввод числа

--- complex.c
+++ complex.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main(){
-    printf("F");
-    printf("respect");
+    double n,z;
+    int inp;
+    // ввод числа
+    printf("Введите вещественную часть числа:");
+    scanf("%lf",&n);
+    printf("Введите мнимую часть числа:");
+    scanf("%lf",&z);
+    printf("Ваше число:\t"); printf("%lf",n); printf("+"); printf("%lf",z); printf("i");
     return 0;
 }
