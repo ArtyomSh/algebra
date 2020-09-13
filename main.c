4c06a5456086929516a40e4f7bcc28c547a50c97
dan-taranetc <71114728+dan-taranetc@users.noreply.github.com>
2020-09-13 12:38:51 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  complex
+//
+//  Created by Данил Таранец on 13.09.2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
