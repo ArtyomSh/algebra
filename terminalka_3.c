93883f318201d94c0c78e5092a2dc61120ec4536
dan-taranetc <71114728+dan-taranetc@users.noreply.github.com>
2020-10-25 19:17:24 +0700

    Ввод векторов

--- terminalka_3.c
+++ terminalka_3.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    int k;
+    printf("Введите количество координат у вектора:");
+    scanf("%d",&n);
+    printf("Введите количество векторов:");
+    scanf("%d",&k);
+    int** A = (int**)malloc(k*sizeof(int*));
+    for(int i = 0; i<k; i++){
+        A[i] = (int*)malloc(n*sizeof(int));
+    }
+    printf("Введите координаты векторов:\n");
+    for(int i = 0; i<k; i++){
+        for(int j = 0; j<n; j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+}
