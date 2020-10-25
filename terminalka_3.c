b7ce6e29eabfd23c2190af03f6ce2ee1fd9bc517
dan-taranetc <71114728+dan-taranetc@users.noreply.github.com>
2020-10-25 20:34:41 +0700

    Приведение к ступенчатому виду

--- terminalka_3.c
+++ terminalka_3.c
@@ -7,14 +7,25 @@ int main(){
     scanf("%d",&n);
     printf("Введите количество векторов:");
     scanf("%d",&k);
-    int** A = (int**)malloc(k*sizeof(int*));
+    float** A = (float**)malloc(k*sizeof(float*));
     for(int i = 0; i<k; i++){
-        A[i] = (int*)malloc(n*sizeof(int));
+        A[i] = (float*)malloc(n*sizeof(float));
     }
     printf("Введите координаты векторов:\n");
     for(int i = 0; i<k; i++){
         for(int j = 0; j<n; j++){
-            scanf("%d",&A[i][j]);
+            scanf("%f",&A[i][j]);
         }
+    }
+    // приведение к ступенчатому виду
+    int m = 0;
+    for(int q = 0; q<k-1; q++){
+        for(int i = m; i<k-1;i++){
+            float koef = -(A[i+1][m]/(float)A[m][m]);
+            for(int j = m; j<n; j++){
+                A[i+1][j] = A[m][j]*koef + A[i+1][j];
+            }
+        }
+        m += 1;
     }
 }
